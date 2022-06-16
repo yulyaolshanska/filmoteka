fe4ede1420455f37898c634758932aa07651cfd1
SerhiiDubovets <Sergio.d@bigmir.net>
2022-06-16 20:53:55 +0300

    1

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -8,6 +8,7 @@ refs.homeLink.addEventListener('click', onHome);
 refs.logo.addEventListener('click', onHome);
 
 function onHome() {
+  onLoader();
   refs.homeLink.classList.add('site-nav__link--current');
   refs.myLibraryLink.classList.remove('site-nav__link--current');
   refs.buttonsBlock.classList.add('header__block-btn--is-hidden');
