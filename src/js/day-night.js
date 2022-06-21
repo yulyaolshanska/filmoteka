15138f62bac29bde2cc2f98e642d81761ac8f260
SerhiiDubovets <Sergio.d@bigmir.net>
2022-06-21 20:23:15 +0300

    1

--- src/js/day-night.js
+++ src/js/day-night.js
@@ -8,18 +8,31 @@ document.querySelector('.themetoggle').addEventListener('click', event => {
   addDarkClassToHTML();
 });
 
-function addDarkClassToHTML() {
+export function addDarkClassToHTML() {
   try {
     if (localStorage.getItem('theme') === 'dark') {
       document.querySelector('body').classList.add('dark');
-      // document.querySelector('main').classList.add('dark');
-      document.querySelector('footer').classList.add('dark');
+      document
+        .querySelector('.container--bg-color')
+        .classList.add('container--bg-color-dark');
+      document.querySelector('.film-name').classList.add('film-name--dark');
+      document
+        .querySelector('.film-description')
+        .classList.add('film-description--dark');
+      document.querySelector('.film-name').classList.add('film-name--dark');
+
+      // document.querySelector('footer').classList.add('dark');
       document.querySelector('.themetoggle span').textContent = 'dark_mode';
     } else {
       document.querySelector('body').classList.remove('dark');
+      document
+        .querySelector('.container--bg-color')
+        .classList.remove('container--bg-color-dark');
+      document
+        .querySelector('.film-description')
+        .classList.remove('film-description--dark');
+      document.querySelector('.film-name').classList.remove('film-name--dark');
       document.querySelector('.themetoggle span').textContent = 'wb_sunny';
     }
   } catch (err) {}
 }
-
-addDarkClassToHTML();
--- src/js/start.js
+++ src/js/start.js
@@ -1,48 +1,43 @@
 import NewsApiService from './api-service';
 import trendResultList from '../templates/poster.hbs';
+import { addDarkClassToHTML } from './day-night';
 
 export default class Start extends NewsApiService {
   constructor() {
     super();
-   this.upButton = super.getRefs().upButton;
-   this.height = window.innerHeight;   
+    this.upButton = super.getRefs().upButton;
+    this.height = window.innerHeight;
   }
 
   async renderMainCollection(genres) {
     const finalData = await this.getData(genres);
-   
+
     super.getRefs().filmsContainer.innerHTML = trendResultList(finalData);
+    addDarkClassToHTML();
   }
 
   async getData(genres) {
-  
     try {
-    
       super.resetPage();
-  
+
       const data = await super.fetchTrend();
-      
       return super.getFinalData(data, genres);
-      
     } catch (error) {
-      console.dir(error)
+      console.dir(error);
     }
   }
 
-  addButtonUp(){
+  addButtonUp() {
+    window.addEventListener('scroll', () => {
+      this.height > scrollY
+        ? (this.upButton.className = 'up-button--is-hidden')
+        : (this.upButton.className = 'up-button');
+    });
 
-    window.addEventListener('scroll', ()=> {
-      this.height > scrollY ? this.upButton.className ='up-button--is-hidden': this.upButton.className ='up-button' ; 
-      } );
+    this.upButton.addEventListener('click', () => {
+      window.scrollBy({ top: -this.height - scrollY, behavior: 'smooth' });
+    });
 
-      this.upButton.addEventListener('click', ()=>{
-        window.scrollBy({top: -this.height-scrollY, behavior: "smooth",})
-      });
-   
-
-   return 
+    return;
   }
-
 }
-
-
