45e42d0afea4509fd7863f3607f38ad4fc8f98ef
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-06-22 22:59:57 +0300

    Цвет текста с темной темой

--- src/js/library.js
+++ src/js/library.js
@@ -66,7 +66,15 @@ export default class Library extends Modal {
     this.queueBtn.classList.remove('header__btn--active');
     this.status = 'watched';
     this.observerItem.dataset.observe = 'watched';
-     
+     if (localStorage.getItem('theme') === 'dark') {
+      
+              console.log(document
+        .querySelector('.nothing'));
+
+           document
+        .querySelector('.nothing')
+        .classList.add('nothing--dark');
+     }
     if (this.watchedFilms.length === 0) {
       this.filmsContainer.innerHTML = `<li class='nothing'>Sorry, but you didn't add any films in your Watched category yet</li>`;
 
@@ -82,7 +90,11 @@ export default class Library extends Modal {
     this.watchedBtn.classList.remove('header__btn--active');
     this.status = 'queue';
     this.observerItem.dataset.observe = 'queue';
-        
+      if (localStorage.getItem('theme') === 'dark') {
+           document
+        .querySelector('.nothing')
+        .classList.add('nothing--dark');
+     }   
     if (this.queue.length === 0) {
       this.filmsContainer.innerHTML = `<li class='nothing'>Sorry, but you didn't add any films in your Queue category yet</li>`;
 
